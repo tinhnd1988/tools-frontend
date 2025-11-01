ede054a18887fa8d17f44c6f7446d27af31fe2fb
Tinh Nguyen <tinhnguyenduc1988@gmail.com>
2025-11-01 11:12:56 +0700

    refactor: remove OptimizedImage component and replace its usage with standard img tags in Sidebar and Donate pages

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -1,20 +1,13 @@
 import { useTranslation } from 'react-i18next';
 import qrImg from '../assets/qr.jpg';
-import OptimizedImage from '../components/OptimizedImage';
 
 export default function Donate() {
   const { t } = useTranslation();
   return (
     <div className="space-y-4 text-center">
       <div className="text-sm opacity-70">{t('donate_desc')}</div>
-      <div className="rounded-xl inline-block bg-white text-center p-4">
-        <OptimizedImage 
-          src={qrImg} 
-          alt="Donate QR Code" 
-          width={300} 
-          height={300}
-          className="max-w-full h-auto mx-auto object-contain" 
-        />
+      <div className="rounded-xl inline-block bg-white text-center">
+        <img src={qrImg} alt="Donate QR" className="w-1/2 object-cover inline-block" />
       </div>
     </div>
   );
